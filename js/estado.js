5049ea1c9b5b85434a83cc7b3e10b8f279edd896
h3cjun <62990101+h3cjun@users.noreply.github.com>
2020-12-18 17:45:39 +0100

    Update estado.js

--- js/estado.js
+++ js/estado.js
@@ -3,7 +3,7 @@ fetch("https://eu.mc-api.net/v3/server/ping/144.91.105.97:32540")
     return response.json();
   })
   .then(function (myJson) {
-    document.querySelector("#estado").innerHTML = myJson.online ? 'ColorCraft se encuentra abierto' : 'ColorCraft se encuentra  cerrado';
+    document.querySelector("#estado").innerHTML = myJson.online ? 'ColorCraft está abierto' : 'ColorCraft está cerrado';
 		document.querySelector("#njug").innerHTML = myJson.online ? 'Jugadores online: '+myJson.players.online : '';
 		document.querySelector("#online").innerHTML = myJson.online ? myJson.players.online : '';
 		document.querySelector("#ipText").innerHTML = myJson.players.max;
